beeaa869b724116547f45ee2a7f21cfe6f753d21
millenagualandi <millenagualandi@gmail.com>
2025-05-14 15:33:06 -0300

    segunda etapa concluida

---
+++ data/categoria_model.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Categoria:
+    id: int
+    nome: str
+    
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import uvicorn
 
 from data import produto_repo
 from data import cliente_repo
+from data import categoria_repo
 
 
 app = FastAPI()
@@ -13,6 +14,7 @@ templates = Jinja2Templates(directory="templates")
 
 produto_repo.criar_tabela()
 cliente_repo.criar_tabela()
+categoria_repo.criar_tabela()
 
 
 @app.get("/")
@@ -34,6 +36,11 @@ async def get_produtos():
     response = templates.TemplateResponse("produtos.html", {"request": {}, "produtos": produtos})
     return response
 
+@app.get("/categorias")
+async def get_categorias():
+    categorias = categoria_repo.obter_todos()
+    response = templates.TemplateResponse("categoria.html", {"request": {}, "categorias": categorias})
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
